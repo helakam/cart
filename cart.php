614a843630c55bcc223f0d19dac7204101f93948
praveen9812 <54749786+praveen9812@users.noreply.github.com>
2021-03-28 23:36:20 +0530

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+require_once ("php/CreateDb.php");
+require_once ("php/component.php");
+
+$db = new CreateDb("newdb", "product");
+
+if (isset($_POST['remove'])){
+  if ($_GET['action'] == 'remove'){
+      foreach ($_SESSION['cart'] as $key => $value){
+          if($value["product_id"] == $_GET['id']){
+              unset($_SESSION['cart'][$key]);
+              //echo "<script>alert('Product has been Removed...!')</script>";
+              echo "<script>window.location = 'cart.php'</script>";
+          }
+      }
+  }
